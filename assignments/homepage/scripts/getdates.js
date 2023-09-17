90a41f40d9eb29241b6421fe53a0a1e90334bce0
V3rilen <asher@verilen.com>
2023-09-16 22:05:36 -0600

    Added the css and js scripts

--- assignments/homepage/scripts/getdates.js
+++ assignments/homepage/scripts/getdates.js
@@ -1,4 +1,13 @@
 let today1 = new Date()
+let visitcount = parseInt(localStorage.getItem("visitcount"))
+if(visitcount){
+    visitcount += 1
+    localStorage.setItem("visitcount", visitcount)
+    document.querySelector("#pagevisits").textContent = visitcount
+}else{
+    localStorage.setItem("visitcount",1)
+}
 
 document.querySelector("#currentyear").textContent = today1.getFullYear()
 document.querySelector("#lastmodified").textContent = document.lastModified
+
