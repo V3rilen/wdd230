fbeb0326f480b64ee30b8f468f7cfc1e2df922d2
V3rilen <asher@verilen.com>
2023-10-16 20:17:53 -0600

    Added basic dark mode button

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -2,11 +2,41 @@ const navMenuButtons = document.querySelectorAll(".nav-menu-button");
 const navMenu = document.querySelector(".nav-menu");
 let spotlightSlideShowIndex = 0;
 let navMenuOpen = false;
+let darkModeEnabled = false;
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
 
 navMenuButtons.forEach((button) => {
   button.addEventListener("click", () => {
     navMenu.classList.toggle("open");
   });
+});
+
+darkModeToggle.addEventListener("click", () => {
+  if (darkModeEnabled) {
+    darkModeToggle.innerHTML = "Light";
+    document.documentElement.style.setProperty(
+      "--main-background-color",
+      "#ffffff"
+    );
+    document.documentElement.style.setProperty(
+      "--main-text-color",
+      "#353535ff"
+    );
+    darkModeEnabled = false;
+  } else {
+    darkModeToggle.innerHTML = "Dark";
+    document.documentElement.style.setProperty(
+      "--main-background-color",
+      "#353535ff"
+    );
+    document.documentElement.style.setProperty(
+      "--main-text-color",
+      "#d9d9d9ff"
+    );
+    darkModeEnabled = true;
+  }
+
+  // document.style.setProperty("--alternate-highlight", "#d9d9d9ff");
 });
 
 let today1 = new Date();
