084b8e34ec1b79c9d86417a2ce31818930df8f40
V3rilen <asher@verilen.com>
2023-10-19 16:40:46 -0600

    Added dark mode and weather section to chamber

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -3,7 +3,7 @@ const navMenu = document.querySelector(".nav-menu");
 let spotlightSlideShowIndex = 0;
 let navMenuOpen = false;
 let darkModeEnabled = false;
-const darkModeToggle = document.querySelector(".dark-mode-toggle");
+const darkModeToggle = document.querySelector(".dark-mode-toggle input");
 
 navMenuButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -11,7 +11,7 @@ navMenuButtons.forEach((button) => {
   });
 });
 
-darkModeToggle.addEventListener("click", () => {
+darkModeToggle.addEventListener("change", () => {
   if (darkModeEnabled) {
     darkModeToggle.innerHTML = "Light";
     document.documentElement.style.setProperty(
@@ -52,3 +52,25 @@ let today1 = new Date();
 
 document.querySelector("#currentyear").textContent = today1.getFullYear();
 document.querySelector("#lastmodified").textContent = document.lastModified;
+
+const temperatureSpan = document.querySelector("#temperature");
+const windspeedSpan = document.querySelector("#windspeed");
+const windchillSpan = document.querySelector("#windchill");
+const temperature = parseInt(temperatureSpan.textContent);
+const windspeed = parseFloat(windspeedSpan.textContent);
+
+function showWindChill(temp, speed) {
+  message = "N/A";
+  if (temp <= 50 && speed > 3.0) {
+    let chill =
+      35.74 +
+      0.6215 * temp -
+      35.75 * Math.pow(speed, 0.16) +
+      0.4275 * temp * Math.pow(speed, 0.16);
+    message = `${Math.round(chill)}`;
+    console.log(message);
+  }
+  windchillSpan.textContent = message;
+}
+
+showWindChill(temperature, windspeed);
