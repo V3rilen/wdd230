31322237ada713442aae451697dd4c4922cd451f
V3rilen <asher@verilen.com>
2023-11-08 17:46:00 -0700

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,7 +2,7 @@ const getMemberData = async () => {
   const data = await fetch("./data/members.json").then((response) =>
     response.json()
   );
-  console.log(response);
+  console.log(data);
 };
 
 getMemberData();
