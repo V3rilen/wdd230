4c3010c29aba56d52aa3eb7da9b846a51beabe75
V3rilen <asher@verilen.com>
2023-11-09 15:01:50 -0700

    Updated directory page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,8 +1,42 @@
+// import data from "./data/members.json";
+const memberCardHolder = document.querySelector("#member-cards-holder");
 const getMemberData = async () => {
   const data = await fetch("./data/members.json").then((response) =>
     response.json()
   );
-  console.log(data);
+  const members = data;
+  members.forEach((member) => {
+    const cardTemplate = `
+        <header>
+        <a href="${member.website}" target="_blank"><img src="${
+      member.icon
+    }" height=40 alt="Icon for ${member.name}" loading="lazy"/></a> 
+        <span>${member.name}</span>
+        </header>
+        <section>
+          <ul>
+            <li>
+            <span>Address: ${member.address}</span>
+            </li>
+            ${
+              member.phone &&
+              `<li>
+                  <span>Phone: ${member.phone}</span>
+                </li>`
+            }
+            
+            <li>
+            <span>Membership Level: ${member.memberLevel}</span>
+            </li>
+          </ul>
+        </section>
+        
+  `;
+    const newCard = document.createElement("section");
+    newCard.classList.add("card");
+    newCard.innerHTML = cardTemplate;
+    memberCardHolder.appendChild(newCard);
+  });
 };
 
 getMemberData();
